54aaf97732857722ddce271e376152b727b77a76
Micael Bittencourt <ti.micaelbittenourt@outlook.com>
2019-01-23 12:12:07 -0200

    Foi addicionado a classe SavingAccont e modificado o Program

--- ProjectAccount/Program.cs
+++ ProjectAccount/Program.cs
@@ -7,12 +7,34 @@ namespace ProjectAccount
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(5509, "Bob Brown", 100.0, 500.0);
+            Account acc = new Account(5509, "Bobison", 0.0);
+            BusinessAccount bacc = new BusinessAccount(5905, "Joaquinaina", 0.0, 500.0);
 
-            Console.WriteLine(account.Balance);
+            // UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Joaquinsom", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anitanita", 0.0, 0.01);
 
-            //error
-            //account.Balance = 200.0;
+            // DOWNCASTING
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            // BusinessAccount acc5 = (BusinessAccount)acc3;
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
