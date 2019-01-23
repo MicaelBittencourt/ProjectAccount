975a8327ea7a55600c65abfc082990984711c25e
Micael Bittencourt <ti.micaelbittenourt@outlook.com>
2019-01-23 11:11:23 -0200

    Projeto inicial com duas classes, Account e BussinessAccount. Testada e Funcionando. Aula de Herança e Poliformismo

---
+++ ProjectAccount/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectAccount.Entities
+{
+    class Account
+    {
+        public int Number  { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void Withdraw(double amont)
+        {
+            Balance -= amont;
+        }
+        public void Deposit(double amont)
+        {
+            Balance += amont;
+        }
+    }
+}
---
+++ ProjectAccount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ProjectAccount.Entities;
+
+namespace ProjectAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(5509, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //error
+            //account.Balance = 200.0;
+        }
+    }
+}
